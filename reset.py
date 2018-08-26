b726ca11aab5f87723eef763c5d0cf3dc3f106b2
Amit Joki <amitjoki@gmail.com>
2018-08-26 10:46:08 +0530

    Update reset.py

--- reset.py
+++ reset.py
@@ -2,5 +2,6 @@ import model
 from collections import defaultdict
 import all
 
-model.save_obj(defaultdict(int, all.all_words), 'data')
-model.save_obj(defaultdict(int, {}), 'errors')
+def reset():
+    model.save_obj(defaultdict(int, all.all_words), 'data')
+    model.save_obj(defaultdict(int, {}), 'errors')
